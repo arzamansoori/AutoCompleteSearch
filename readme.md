513f0648ce9f224588fc73d61f3e483fdf1495e3
arzatf <arzamansoori786@gmail.com>
2022-10-31 15:13:52 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,29 @@
+![AUTOCOMPLETESEARCH](https://img.shields.io/badge/AUTOCOMPLETE-SEARCH-%23D291BC)
+
+AutoComplete Search website is created with using HTML, CSS(styling yet to be done) and JavaScript.
+
+## TABLE OF CONTENTS
+#
+* General Info
+* Technologies
+* Launch
+
+### General Info
+#
+AutoComplete Search website displays Anime series relevant names as soon as you start typing in the box. If your typed name isn't on the list. It won't display anything.
+
+### Technologies
+#
+* HTML
+* CSS(yet to be done)
+* JavaScript
+
+### Launch
+#
+To use AutoComplete Search website or to see the project. Please click on the deployed link below.
+
+[AutoComplete Search](https://wordcountwebsite.netlify.app/)
+<br>
+<br>
+<!-- ![STOPWATCH Website Image](./stopwatch.png) -->
+
---
+++ script.js
@@ -0,0 +1,46 @@
+const input = document.getElementById("myInput").value;
+const div = document.getElementById("myDropdown");
+const li = div.getElementsByTagName("li");
+
+if (input.length > 0) {
+    for (let i = 0; i < li.length; i++) {
+        li[i].style.display = "block"
+    }
+}
+else if(input.length = 0) {
+    for (let i = 0; i < li.length; i++) {
+        li[i].style.display = "none";
+    }
+}
+else{
+    for (let i = 0; i < li.length; i++) {
+        li[i].style.display = "none";
+    }
+}
+
+function myFunction() {
+    const input = document.getElementById("myInput");
+    const filter = input.value.toUpperCase();
+    const div = document.getElementById("myDropdown");
+    const li = div.getElementsByTagName("li");
+
+
+    for (let i = 0; i < li.length; i++) {
+        const text = li[i].innerText;
+        if(input.value.length == 0){
+            for(let i=0; i<li.length; i++){
+                li[i].style.display = "none";
+              }
+        }
+        else{
+            if (text.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            }
+            else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+}
+
+
